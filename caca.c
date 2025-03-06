379f5e166acfccce16f02ac7b3a8cea0a6346c3b
Paul R <rp00772w@dell7060test>
2025-03-06 13:00:47 +0100

    Test 1

--- caca.c
+++ caca.c
@@ -1,31 +1,51 @@
 #include "types.h"
 
-cases *cases_vide_adj(plateau p, cases c){
-    
-    int i, j;
+/* FAIRE UNE FONCTION C_*AC */
 
-    for (i=-1; i <= 1; i++){
-        for (j=-1; j <= 1; j++){
+int suite(int x, int y, int i, int j, int couleur){
 
-            if (
-            
-        
-        }
-    }
-
-    
+    while (((x+=i) >= 0 && x <= p->n)
+           && ((y+=j) >= 0 && y <= p->n)
     
 }
 
-cases *verif_case(plateau p, int couleur){
+cellule *cases_vide_adj(plateau p, int x, int y){
+    
+    int i, j,
+        c_adv;
 
-    int i, j, nb_c_vide;
-    cases *c_vide;
+    c_adv = couleur_adverse(c_j);
+    
+    /* controle des cellules voisines de c */
+    for (i=-1; i <= 1; i++){
+        for (j=-1; j <= 1; j++){
 
+            /* verification des coordonnees pour ne pas se retrouver en dehors du plateau et ne pas controler la cellule (x,y) */
+            if ((x+i >= 0 && x+i <= p->n)
+                && (y+j >= 0 && y+j <= p->n)
+                && !(i == 0 && j == 0)){
+
+                /* si une cellule voisine correspond a la couleur adverse alors on regarde les suivantes dans la meme direction */
+                if (p->mat[x+i][y+j] == c_adv){
+                    suite()
+                }
+            }
+        }
+    }
+}
+
+cellule *verif_case(plateau p, int c_j){
+
+    int i, j,
+        nb_c_vide;
+    cellule *c_vide;
+    
+    /* parcours de chaque cellules du plateau */
     for (i=0; i < p->n; i++){
         for (j=0; j < p->n; j++){
 
-            if (p->m[i][j] == couleur){
+            /* on cherche des cellules de la couleur du joueur */
+            if (p->m[i][j] == c_j){
 
                 cases_vide_adj(p->m[i][j]);
                 
