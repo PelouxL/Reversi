a0d3f990204338bd444a0c3d07defa88b901cf05
Paul RODRIGUES <147482873+Zurakooooo@users.noreply.github.com>
2025-03-20 14:07:10 +0100

    Add files via upload
    modif plateau.h et supp de types.h

--- plateau.h
+++ plateau.h
@@ -1,7 +1,16 @@
 #ifndef _PLATEAU_H_
 #define _PLATEAU_H_
 
-#include "types.h"
+#define VIDE 0
+#define NOIR 1
+#define BLANC 2
+
+typedef struct{
+  int **mat; /* matrice carre ? */
+  int n;
+  int j_couleur;
+  int ordi_cordi;
+}plateau; /* variable globale ? */
 
 plateau *initialisation_plateau(int n);
 
